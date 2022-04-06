a8978fcefb9220c4446cd59602d65ccac3edcdd3
juy4556 <kws05050@naver.com>
2022-04-06 11:42:34 +0900

    22.04.06

--- 백준/6064 카잉 달력.py
+++ 백준/6064 카잉 달력.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     T = int(input())
     for _ in range(T):
         M, N, x, y = map(int, input().split())
-        while x < M * N:
+        end = M*N
+        while x <= end:
             if (x - y) % N == 0:
                 break
             x += M
-        if x > M * N:
+        if x > end:
             print(-1)
             continue
         print(x)
