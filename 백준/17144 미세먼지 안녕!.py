b56ba78c50d28ad1f79a5cc5ad201440ee8aec55
juy4556 <kws05050@naver.com>
2022-06-18 23:38:00 +0900

    22.06.18

--- 백준/17144 미세먼지 안녕!.py
+++ 백준/17144 미세먼지 안녕!.py
@@ -1,7 +1,13 @@
 R, C, T = map(int, input().split())
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 space = []
 for _ in range(R):
     space.append(list(map(int, input().split())))
 
-def diffusion():
-    #ha..
+def diffusion(r, c):
+
+
+def solution():
+    for i in range(R):
+        for j in range(C):
