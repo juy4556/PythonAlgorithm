bdba18545a09c6a95afe5cf82e8a9a6f18084e54
juy4556 <kws05050@naver.com>
2022-06-26 23:50:13 +0900

    22.06.26

---
+++ 백준/14890 경사로.py
@@ -0,0 +1,7 @@
+N, L = map(int, input().split())
+space = []
+for _ in range(N):
+    space.append(list(map(int, input().split())))
+
+
+def solution():
---
+++ 알고리즘/2차원 리스트 시계, 반시계 방향 회전.py
@@ -0,0 +1,10 @@
+# 2차원 리스트를 90도 회전한 결과를 반환하는 함수
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a) # 행 길이
+    m = len(a[0]) # 열 길이
+    result = [[0] * n for _ in range(m)] # 결과 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n-i-1] = a[i][j]  # 반시계는 result[m-j-1][i] = a[i][j]
+    return result
+
