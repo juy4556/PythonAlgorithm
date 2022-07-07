0cc4fb2091098a4edb41fb54a7c93643a4e20c20
juy4556 <kws05050@naver.com>
2022-07-07 23:57:29 +0900

    22.07.07

--- 백준/2048 (Easy).py
+++ 백준/2048 (Easy).py
@@ -9,17 +9,19 @@ def left(arr):
     for i in range(N):  # 행
         j = 0
         k = j + 1
-        flag = False
         while j < N and k < N:
-            if arr[i][j] >= 2 and arr[i][j] == arr[i][k]:
+            if arr[i][k] == 0:
+                k += 1
+                continue
+            if arr[i][j] != arr[i][k]:
+                j += 1
+                k = j
+            elif arr[i][j] == arr[i][k]:
                 arr[i][j] *= 2
                 arr[i][k] = 0
                 j = k + 1
                 k = j
-                flag = True
             k += 1
-            if not flag:
-                j += 1
         temp = []
         for l in range(N):
             if arr[i][l] != 0:
@@ -35,17 +37,20 @@ def right(arr):
     for i in range(N):  # 행
         j = N - 1
         k = j - 1
-        flag = False
         while j >= 0 and k >= 0:
-            if arr[i][j] >= 2 and arr[i][j] == arr[i][k]:
+            if arr[i][k] == 0:
+                k -= 1
+                continue
+            if arr[i][j] != arr[i][k]:
+                j -= 1
+                k = j
+            elif arr[i][j] == arr[i][k]:
+                print(j, k)
                 arr[i][j] *= 2
                 arr[i][k] = 0
                 j = k - 1
                 k = j
-                flag = True
             k -= 1
-            if not flag:
-                j -= 1
         temp = []
         for l in range(N - 1, -1, -1):
             if arr[i][l] != 0:
@@ -54,26 +59,28 @@ def right(arr):
         for _ in range(N - length):
             temp.append(0)
         temp.reverse()
+        # print(temp)
         arr[i] = temp
     return arr
 
 
 def up(arr):
-    print(arr)
     for i in range(N):  # 열
         j = 0
         k = j + 1
-        flag = False
         while j < N and k < N:
-            if arr[j][i] >= 2 and arr[j][i] == arr[k][i]:
+            if arr[k][i] == 0:
+                k += 1
+                continue
+            if arr[j][i] != arr[k][i]:
+                j += 1
+                k = j
+            elif arr[j][i] == arr[k][i]:
                 arr[j][i] *= 2
                 arr[k][i] = 0
                 j = k + 1
                 k = j
-                flag = True
             k += 1
-            if not flag:
-                j += 1
         temp = []
         for l in range(N):
             if arr[l][i] != 0:
@@ -81,11 +88,8 @@ def up(arr):
         length = len(temp)
         for _ in range(N - length):
             temp.append(0)
-        print(temp)
         for l in range(N):
             arr[l][i] = temp[l]
-        for l in range(N):
-            print(arr[l][i])
     return arr
 
 
@@ -93,17 +97,19 @@ def down(arr):
     for i in range(N):  # 열
         j = N - 1
         k = j - 1
-        flag = False
         while j >= 0 and k >= 0:
-            if arr[j][i] >= 2 and arr[j][i] == arr[k][i]:
+            if arr[k][i] == 0:
+                k -= 1
+                continue
+            if arr[j][i] != arr[k][i]:
+                j -= 1
+                k = j
+            elif arr[j][i] == arr[k][i]:
                 arr[j][i] *= 2
                 arr[k][i] = 0
                 j = k - 1
                 k = j
-                flag = True
             k -= 1
-            if not flag:
-                j -= 1
         temp = []
         for l in range(N):
             if arr[l][i] != 0:
@@ -117,19 +123,23 @@ def down(arr):
     return arr
 
 
-def dfs(arr, M):
-    # print(arr)
+def dfs(array, M):
     global result
+    # for l in range(N):
+    #     for m in range(N):
+    #         print(arr[l][m], end=' ')
+    #     print()
+    # print("===========================")
     if M == 5:
         max_num = 0
         for i in range(N):
-            max_num = max(max_num, max(arr[i]))
+            max_num = max(max_num, max(array[i]))
         result = max(result, max_num)
         return
-    dfs(left(arr), M + 1)
-    dfs(right(arr), M + 1)
-    dfs(up(arr), M + 1)
-    dfs(down(arr), M + 1)
+    dfs(left(array), M + 1)
+    dfs(right(array), M + 1)
+    dfs(up(array), M + 1)
+    dfs(down(array), M + 1)
 
 
 def solution():
