336d0e017e75f3658ce4f7fa773c214932453373
juy4556 <kws05050@naver.com>
2022-08-31 16:23:17 +0900

    22.08.30

---
+++ 백준/12865 평범한배낭 복습3.py
@@ -0,0 +1,16 @@
+N, K = map(int, input().split())
+items = []
+for _ in range(N):
+    weight, value = map(int, input().split())
+    items.append([weight, value])
+
+bag = [[0 for _ in range(K+1)] for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        if j >= items[i-1][0]:
+            bag[i][j] = max(bag[i-1][j], items[i-1][1]+bag[i][j-items[i-1][0]])
+        else:
+            bag[i][j] = bag[i-1][j]
+
+print(bag[N][K])
---
+++ 백준/1750 서로소의 개수.py
@@ -0,0 +1,27 @@
+N = int(input())
+S = []
+for i in range(N):
+    num = int(input())
+    S.append(num)
+result = 0
+max_num = max(S)
+dp = [[0 for _ in range(max_num+1)] for _ in range(N+1)]
+for i in range(1, len(S)+1):
+    dp[i][S[i-1]] += 1
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+for i in range(1,N+1):
+    for j in range(1, max_num+1):
+        dp[i][j] += dp[i-1][j]
+        dp[i][j] %= 10000003
+        GCD = gcd(S[i-1], j)
+        dp[i][GCD] += dp[i-1][j]
+        dp[i][GCD] %= 10000003
+print(dp[N][1])
