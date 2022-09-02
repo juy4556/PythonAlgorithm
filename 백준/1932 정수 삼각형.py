bd8040f87d67c6621fb2b53cff77cf0031ca2343
juy4556 <kws05050@naver.com>
2022-09-02 15:11:34 +0900

    22.09.02

---
+++ 백준/1932 정수 삼각형.py
@@ -0,0 +1,18 @@
+N = int(input())
+triangle = []
+for _ in range(N):
+    triangle.append(list(map(int, input().split())))
+
+dp = [[0 for _ in range(N)] for _ in range(N)]
+dp[0][0] = triangle[0][0]
+for i in range(1, N):
+    for j in range(len(triangle[i])):
+        a, b = 0, 0
+        if j==0:
+            dp[i][j] = triangle[i][j] + dp[i-1][j]
+        elif j==len(triangle[i])-1:
+            dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+        else:
+            dp[i][j] = triangle[i][j] + max(dp[i-1][j-1], dp[i-1][j])
+
+print(max(dp[N-1]))
