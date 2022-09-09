809a4c94f5f5f370bd4715105024a9eadfe43cac
juy4556 <kws05050@naver.com>
2022-09-09 22:13:03 +0900

    22.09.09

--- practice.py
+++ practice.py
@@ -1,41 +1,15 @@
-def solution(new_id):
-    new_id = new_id.lower()  # stage1
-    i = 0
-    remove = []
-    dot = []
-    for n in range(len(new_id)):  # stage2
-        if 'a' <= new_id[n] <= 'z':
-            continue
-        if 0 <= ord(new_id[n]) <= 9:
-            continue
-        if new_id[n] == '-' or new_id[n] == '_' or new_id[n] == '.':
-            continue
-        remove.append(n)
-    for i in range(len(remove) - 1, -1, -1):
-        new_id = new_id[:remove[i]] + new_id[remove[i] + 1:]
-    index = 0
-    while index < len(new_id):  # stage3
-        index2 = index
-        while new_id[index2] == '.':
-            index2 += 1
-        if index2 > index:
-            dot.append([index, index2])
-    dot.sort(key=lambda x: -x[0])
-    for s, e in dot:
-        new_id = new_id[:s + 1] + new_id[e:]
-    new_id.strip('.')  # stage4
-    if len(new_id) == 0:  # stage5
-        new_id.append(a)
-    if len(new_id) >= 16:  # stage6
-        new_id = new_id[:16]
-        if new_id[15] == '.':
-            new_id = new_id[:15]
-    if len(new_id) <= 2:  # stage7
-        last = new_id[len(new_id) - 1]
-        while len(new_id) <= 3:
-            new_id = new_id + last
+import heapq
+q=[]
+heapq.heappush(q, (1,0))
+heapq.heappush(q, (0,1))
+heapq.heappush(q, (7,2))
+heapq.heappush(q, (5,3))
+heapq.heappush(q,(11,4))
+visited = [0] * 5
+visited[2] = 1
+while q and visited[q[0][1]] == 0:
+    heapq.heappop(q)
+if q:
+    heapq.heappop(q)
 
-    return new_id
-
-
-print(solution("...!@BaT#*..y.abcdefghijklm"))
+print(q)
---
+++ 백준/7662 이중 우선순위 큐.py
@@ -0,0 +1,36 @@
+import heapq
+
+T = int(input())
+
+for t in range(T):
+    min_q, max_q = [], []
+    k = int(input())
+    visited = [0] * (k + 1)
+    for i in range(k):
+        op, num = input().split()
+        num = int(num)
+        if op == 'I':
+            heapq.heappush(min_q, (num, i))
+            heapq.heappush(max_q, (-num, i))
+            visited[i] = 1
+        elif op == 'D':
+            if num == 1:  # 최댓값 삭제
+                while max_q and visited[max_q[0][1]] == 0:
+                    heapq.heappop(max_q)
+                if max_q:
+                    visited[max_q[0][1]]=False
+                    heapq.heappop(max_q)
+            elif num == -1:  # 최솟값 삭제
+                while min_q and visited[min_q[0][1]] == 0:
+                    heapq.heappop(min_q)
+                if min_q:
+                    visited[min_q[0][1]]=False
+                    heapq.heappop(min_q)
+    while max_q and visited[max_q[0][1]] == 0:
+        heapq.heappop(max_q)
+    while min_q and visited[min_q[0][1]] == 0:
+        heapq.heappop(min_q)
+    if max_q and min_q:
+        print(-heapq.heappop(max_q)[0], heapq.heappop(min_q)[0])
+    else:
+        print("EMPTY")
