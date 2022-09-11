add0f1cbe9b9bd7b4a5f0dceed7b581a2d75c91c
juy4556 <kws05050@naver.com>
2022-09-11 21:07:31 +0900

    22.09.11

---
+++ 백준/10986 나머지 합.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+arr = [0] * N
+sub_sum = 0
+rest = [0] * 1000
+result = 0
+
+for i in range(N):
+    sub_sum += A[i]
+    arr[i] = sub_sum
+    rest[arr[i] % M] += 1
+
+result += rest[0]
+for i in range(M):
+    if rest[i] > 1:
+        result += rest[i]*(rest[i]-1)//2
+print(result)
