64f0f522976fd29f486ef0af1ce6b46340b188f2
juy4556 <kws05050@naver.com>
2022-09-13 19:31:31 +0900

    22.09.13

---
+++ 백준/11048 이동하기.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+space = []
+for _ in range(N):
+    space.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        a,b,c = 0,0,0
+        if i>0 and space[i-1][j]:
+            a = space[i-1][j]
+        if j>0 and space[i][j-1]:
+            b = space[i][j-1]
+        if i>0 and j>0 and space[i-1][j-1]:
+            c = space[i-1][j-1]
+        tmp = max(a,b)
+        maximum = max(tmp,c)
+        space[i][j] = space[i][j]+maximum
+
+print(space[N-1][M-1])
