1294a31a62b911842b43186f2c5656958a0fa257
juy4556 <kws05050@naver.com>
2022-09-17 22:54:31 +0900

    22.09.17

--- practice.py
+++ practice.py
@@ -1,13 +1,16 @@
 import heapq
-q=[]
+
+q=q1
 heapq.heappush(q, (1,0))
 heapq.heappush(q, (0,1))
 heapq.heappush(q, (7,2))
 heapq.heappush(q, (5,3))
 heapq.heappush(q,(11,4))
 
-q1 = q
+
+q = []
 print(heapq.heappop(q1))
 print(heapq.heappop(q1))
 print(heapq.heappop(q1))
 print(heapq.heappop(q1))
+
