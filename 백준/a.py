d8e27f8fc6c17b5b810936b2d85342729ba5f255
juy4556 <kws05050@naver.com>
2022-12-20 13:08:22 +0900

    22.12.20

--- 백준/a.py
+++ 백준/a.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
+
 arr = [['A', 10], ['B', 20], ['C', 30], ['A', 40], ['D', 50], ['A', 40]]
 dic = defaultdict(list)
 for i in range(len(arr)):
     dic[arr[i][0]].append([i, arr[i][1]])
 
 print(dic['A'])
-
