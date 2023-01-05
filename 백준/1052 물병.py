dda2fba0334532c89f0df6dc3696ebf4d25eb811
juy4556 <kws05050@naver.com>
2023-01-05 09:50:46 +0900

    23.01.05

--- 백준/1052 물병.py
+++ 백준/1052 물병.py
@@ -1,10 +1,11 @@
 import sys
+
 input = sys.stdin.readline
 N, K = map(int, input().split())
 
 i = 0
 while True:
-    if bin(N+i).count('1') <= K:
+    if bin(N + i).count('1') <= K:
         print(i)
         break
     else:
