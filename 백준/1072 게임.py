909eec7ab18bf49593dc59d9b33c72d2293ff089
juy4556 <kws05050@naver.com>
2023-01-07 11:10:18 +0900

    23.01.07

--- 백준/1072 게임.py
+++ 백준/1072 게임.py
@@ -1,18 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 x, y = map(int, input().split())
-z = y*100 // x
+z = y * 100 // x
 start, end = 1, x
 if z >= 99:
     print(-1)
 else:
     result = 0
     while start <= end:
-        mid = (start+end) // 2
+        mid = (start + end) // 2
 
-        if (y+mid)*100//(x+mid) <= z:
-            start = mid+1
+        if (y + mid) * 100 // (x + mid) <= z:
+            start = mid + 1
         else:
             result = mid
-            end = mid-1
+            end = mid - 1
     print(result)
