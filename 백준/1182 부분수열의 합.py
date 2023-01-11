2a8e3e4f0ac1cb5fa5002e9b9986b694fc341f3b
juy4556 <kws05050@naver.com>
2023-01-11 10:24:01 +0900

    23.01.11

--- 백준/1182 부분수열의 합.py
+++ 백준/1182 부분수열의 합.py
@@ -1,17 +1,21 @@
 import sys
+
 input = sys.stdin.readline
 n, s = map(int, input().split())
 array = list(map(int, input().split()))
 result = 0
-def dfs(i,sum):
+
+
+def dfs(i, sum):
     global result
-    if i>n-1:
+    if i > n - 1:
         return
     sum += array[i]
-    if(sum == s):
+    if (sum == s):
         result += 1
-    dfs(i+1,sum)
-    dfs(i+1,sum-array[i])
+    dfs(i + 1, sum)
+    dfs(i + 1, sum - array[i])
 
-dfs(0,0)
+
+dfs(0, 0)
 print(result)
