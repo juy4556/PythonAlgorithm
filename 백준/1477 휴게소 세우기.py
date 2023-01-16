13f32949fdd195d42b1816bae05d5ee7ab2359a1
juy4556 <kws05050@naver.com>
2023-01-16 23:08:34 +0900

    23.01.16

--- 백준/1477 휴게소 세우기.py
+++ 백준/1477 휴게소 세우기.py
@@ -3,14 +3,14 @@ rest = [0] + list(map(int, input().split())) + [L]
 rest.sort()
 print(rest)
 start = 1
-end = L-1
+end = L - 1
 result = 0
 while start <= end:
     mid = (start + end) // 2
     count = 0
     for i in range(1, len(rest)):
-        count += (rest[i]-rest[i-1]) // mid
-        if (rest[i]-rest[i-1]) % mid == 0:
+        count += (rest[i] - rest[i - 1]) // mid
+        if (rest[i] - rest[i - 1]) % mid == 0:
             count -= 1
 
     if count <= M:
