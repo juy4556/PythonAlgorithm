7263c99fced313399355407715c14d98936db7b7
juy4556 <kws05050@naver.com>
2023-01-23 11:29:31 +0900

    23.01.23

--- 백준/1750 서로소의 개수.py
+++ 백준/1750 서로소의 개수.py
@@ -5,9 +5,9 @@ for i in range(N):
     S.append(num)
 result = 0
 max_num = max(S)
-dp = [[0 for _ in range(max_num+1)] for _ in range(N+1)]
-for i in range(1, len(S)+1):
-    dp[i][S[i-1]] += 1
+dp = [[0 for _ in range(max_num + 1)] for _ in range(N + 1)]
+for i in range(1, len(S) + 1):
+    dp[i][S[i - 1]] += 1
 
 
 def gcd(a, b):
@@ -17,11 +17,11 @@ def gcd(a, b):
         return gcd(b, a % b)
 
 
-for i in range(1,N+1):
-    for j in range(1, max_num+1):
-        dp[i][j] += dp[i-1][j]
+for i in range(1, N + 1):
+    for j in range(1, max_num + 1):
+        dp[i][j] += dp[i - 1][j]
         dp[i][j] %= 10000003
-        GCD = gcd(S[i-1], j)
-        dp[i][GCD] += dp[i-1][j]
+        GCD = gcd(S[i - 1], j)
+        dp[i][GCD] += dp[i - 1][j]
         dp[i][GCD] %= 10000003
 print(dp[N][1])
