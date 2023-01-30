19d27560e661185458bd05b047506c1c860bef76
juy4556 <kws05050@naver.com>
2023-01-30 11:01:21 +0900

    23.01.30

--- 백준/2023 신기한 소수.py
+++ 백준/2023 신기한 소수.py
@@ -2,7 +2,7 @@ def isPrime(num):
     if num == 0 or num == 1:
         return False
     i = 2
-    while i*i <= num:
+    while i * i <= num:
         if num % i == 0:
             return False
         i += 1
