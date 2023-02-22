28247a9a3ad5b1bcd3f059f62499458e7aaf3303
juy4556 <kws05050@naver.com>
2023-02-22 10:18:37 +0900

    23.02.22

--- 백준/7490 0만들기.py
+++ 백준/7490 0만들기.py
@@ -1,8 +1,10 @@
 import sys
+
 input = sys.stdin.readline
 
 tc = int(input())
 array = ['+', ' ', '-']
+
 
 def solution(index):
     global formula
@@ -15,29 +17,30 @@ def solution(index):
     for i in range(3):
         if i == 0:
             result += index
-            formula += array[i]+str(index)
-            solution(index+1)
+            formula += array[i] + str(index)
+            solution(index + 1)
             formula = formula[:-2]
             result -= index
         elif i == 1:
-            formula += array[i]+str(index)
+            formula += array[i] + str(index)
             temp = formula.replace(' ', '')
             result = eval(temp)
             solution(index + 1)
             formula = formula[:-2]
-            temp = formula.replace(' ','')
+            temp = formula.replace(' ', '')
             result = eval(temp)
         else:
             result -= index
-            formula += array[i]+str(index)
-            solution(index+1)
+            formula += array[i] + str(index)
+            solution(index + 1)
             formula = formula[:-2]
             result += index
+
 
 for _ in range(tc):
     n = int(input())
     formula = '1'
-    answer=[]
+    answer = []
     result = 0
     solution(2)
     answer.sort()
