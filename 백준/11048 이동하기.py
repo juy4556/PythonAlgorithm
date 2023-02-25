87fc6f6dfdcde342f39bce511c12f924e61e8f2a
juy4556 <kws05050@naver.com>
2023-02-25 12:06:16 +0900

    23.02.25

--- 백준/11048 이동하기.py
+++ 백준/11048 이동하기.py
@@ -5,15 +5,15 @@ for _ in range(N):
 
 for i in range(N):
     for j in range(M):
-        a,b,c = 0,0,0
-        if i>0 and space[i-1][j]:
-            a = space[i-1][j]
-        if j>0 and space[i][j-1]:
-            b = space[i][j-1]
-        if i>0 and j>0 and space[i-1][j-1]:
-            c = space[i-1][j-1]
-        tmp = max(a,b)
-        maximum = max(tmp,c)
-        space[i][j] = space[i][j]+maximum
+        a, b, c = 0, 0, 0
+        if i > 0 and space[i - 1][j]:
+            a = space[i - 1][j]
+        if j > 0 and space[i][j - 1]:
+            b = space[i][j - 1]
+        if i > 0 and j > 0 and space[i - 1][j - 1]:
+            c = space[i - 1][j - 1]
+        tmp = max(a, b)
+        maximum = max(tmp, c)
+        space[i][j] = space[i][j] + maximum
 
-print(space[N-1][M-1])
+print(space[N - 1][M - 1])
