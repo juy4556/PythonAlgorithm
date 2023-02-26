ce0e85d74dd72b6dc35074acdec33f0493270833
juy4556 <kws05050@naver.com>
2023-02-26 10:56:19 +0900

    23.02.26

--- 백준/11060 점프 점프.py
+++ 백준/11060 점프 점프.py
@@ -1,15 +1,15 @@
 N = int(input())
 A = list(map(int, input().split()))
 dp = [1001] * N
-dp[0]=0
+dp[0] = 0
 for i in range(N):
     jump = A[i]
-    for j in range(1, jump+1):
-        if i+j>=N:
+    for j in range(1, jump + 1):
+        if i + j >= N:
             break
-        dp[i+j] = min(dp[i+j],dp[i]+1)
+        dp[i + j] = min(dp[i + j], dp[i] + 1)
 
-if dp[N-1]<1001:
-    print(dp[N-1])
+if dp[N - 1] < 1001:
+    print(dp[N - 1])
 else:
     print(-1)
