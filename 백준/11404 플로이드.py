62772c01cbeaa3e04dd4680dfb4ec017571211d5
juy4556 <kws05050@naver.com>
2023-03-02 11:31:39 +0900

    23.03.02

--- 백준/11404 플로이드.py
+++ 백준/11404 플로이드.py
@@ -4,15 +4,14 @@ MAX = int(1e9)
 cities = [[MAX for _ in range(n)] for _ in range(n)]
 for _ in range(m):
     start, end, cost = map(int, input().split())
-    cities[start-1][end-1] = min(cities[start-1][end-1], cost)
-
+    cities[start - 1][end - 1] = min(cities[start - 1][end - 1], cost)
 
 for k in range(n):
     for i in range(n):
         for j in range(n):
             if i == j:
                 continue
-            cities[i][j] = min(cities[i][j], cities[i][k]+cities[k][j])
+            cities[i][j] = min(cities[i][j], cities[i][k] + cities[k][j])
 
 for i in range(n):
     for j in range(n):
