038f0296e57e41ac8025bac09109019204612443
juy4556 <kws05050@naver.com>
2023-03-13 10:25:00 +0900

    23.03.13

--- 백준/21608 상어 초등학교(실버1).py
+++ 백준/21608 상어 초등학교(실버1).py
@@ -1,46 +1,47 @@
 import sys
+
 input = sys.stdin.readline
 n = int(input())
 
-student = [[] for _ in range(n**2+1)]
-space = [[0] * (n+1) for _ in range(n+1)]
-dx = [-1, 0, 1, 0] # 상우하좌
+student = [[] for _ in range(n ** 2 + 1)]
+space = [[0] * (n + 1) for _ in range(n + 1)]
+dx = [-1, 0, 1, 0]  # 상우하좌
 dy = [0, 1, 0, -1]
 
-for _ in range(n**2):
+for _ in range(n ** 2):
     a, b, c, d, e = map(int, input().split())
     student[a].append([b, c, d, e])
     x = 1
     y = 1
     max_like = -1
     max_empty = -1
-    for i in range(1, n+1):
-        for j in range(1, n+1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             if space[i][j] == 0:
                 like_count = 0
                 empty_count = 0
                 for k in range(4):
                     nx = i + dx[k]
                     ny = j + dy[k]
-                    if 1<=nx<=n and 1<=ny<=n:
+                    if 1 <= nx <= n and 1 <= ny <= n:
                         if space[nx][ny] in student[a][0]:
                             like_count += 1
                         if space[nx][ny] == 0:
                             empty_count += 1
-                if max_like < like_count or (max_like == like_count and max_empty <empty_count):
+                if max_like < like_count or (max_like == like_count and max_empty < empty_count):
                     x, y = i, j
                     max_like = like_count
                     max_empty = empty_count
     space[x][y] = a
 
 satisfied_sum = 0
-for i in range(1, n+1):
-    for j in range(1, n+1):
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
         count = 0
         for k in range(4):
             nx = i + dx[k]
             ny = j + dy[k]
-            if 1<=nx<=n and 1<=ny<=n:
+            if 1 <= nx <= n and 1 <= ny <= n:
                 if space[nx][ny] in student[space[i][j]][0]:
                     count += 1
         if count == 1:
