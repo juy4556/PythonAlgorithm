2dcf6a83bae10bfe98066e6290753e97276b9c74
juy4556 <kws05050@naver.com>
2023-03-16 23:16:42 +0900

    23.03.16

--- 백준/16234 인구 이동.py
+++ 백준/16234 인구 이동.py
@@ -1,8 +1,9 @@
 from collections import deque
+
 N, L, R = map(int, input().split())
 countries = []
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 for _ in range(N):
     countries.append(list(map(int, input().split())))
 
@@ -24,7 +25,7 @@ while True:
                     nx = x + dx[d]
                     ny = y + dy[d]
                     if 0 <= nx < N and 0 <= ny < N:
-                        if L <= abs(countries[nx][ny]-countries[x][y]) <= R and not visited[nx][ny]:
+                        if L <= abs(countries[nx][ny] - countries[x][y]) <= R and not visited[nx][ny]:
                             flag = 1
                             union.append((nx, ny))
                             visited[nx][ny] = True
