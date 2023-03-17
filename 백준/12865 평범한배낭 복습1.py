803722252b1a81c887c4d8ea9f14626d6957103e
juy4556 <kws05050@naver.com>
2023-03-17 20:33:40 +0900

    23.03.17

--- 백준/12865 평범한배낭 복습1.py
+++ 백준/12865 평범한배낭 복습1.py
@@ -1,5 +1,5 @@
 N, K = map(int, input().split())
-bag = [[0 for _ in range(K+1)] for _ in range(N+1)]
+bag = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 weight = [0]
 value = [0]
 for _ in range(N):
@@ -7,11 +7,11 @@ for _ in range(N):
     weight.append(W)
     value.append(V)
 
-for i in range(1, N+1):
-    for j in range(1, K+1):
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
         if j >= weight[i]:
-            bag[i][j] = max(bag[i-1][j], value[i] + bag[i-1][j-weight[i]])
+            bag[i][j] = max(bag[i - 1][j], value[i] + bag[i - 1][j - weight[i]])
         else:
-            bag[i][j] = bag[i-1][j]
+            bag[i][j] = bag[i - 1][j]
 
 print(bag[N][K])
