21dd44454e8025d0c1a37c66aae23164c983dcfa
juy4556 <kws05050@naver.com>
2023-04-30 19:00:48 +0900

    23.04.30

--- 백준/18353 병사 배치하기.py
+++ 백준/18353 병사 배치하기.py
@@ -5,6 +5,6 @@ dp = [1] * N
 for i in range(1, N):
     for j in range(i):
         if soldier[i] < soldier[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+            dp[i] = max(dp[i], dp[j] + 1)
 
-print(N-max(dp))
+print(N - max(dp))
