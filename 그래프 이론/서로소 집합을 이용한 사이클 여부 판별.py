c02a39d1ced6f3475180d19219c7821129eb0456
juy4556 <kws05050@naver.com>
2023-05-03 11:44:30 +0900

    23.05.03

--- 그래프 이론/서로소 집합을 이용한 사이클 여부 판별.py
+++ 그래프 이론/서로소 집합을 이용한 사이클 여부 판별.py
@@ -2,8 +2,9 @@
 
 def find_parent(parent, x):
     if parent[x] != x:
-        parent[x] = find_parent(parent,parent[x])
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
+
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -13,10 +14,11 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-v, e = map(int, input().split())
-parent = [0] * (v+1)
 
-for i in range(1, v+1):
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
     parent[i] = i
 
 cycle = False
