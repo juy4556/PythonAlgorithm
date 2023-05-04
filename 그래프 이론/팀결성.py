23b6c51a4bf0a52277b2814be046417680c28865
juy4556 <kws05050@naver.com>
2023-05-04 12:14:20 +0900

    23.05.04

--- 그래프 이론/팀결성.py
+++ 그래프 이론/팀결성.py
@@ -1,10 +1,13 @@
 import sys
+
 input = sys.stdin.readline
+
 
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
+
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -14,10 +17,11 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-n, m = map(int, input().split())
-parent = [0] * (n+1)
 
-for i in range(0, n+1):
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+for i in range(0, n + 1):
     parent[i] = i
 
 for _ in range(m):
