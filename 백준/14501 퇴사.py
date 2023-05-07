e17e8fa2bc142b60d693ac9848b23ef9c54ccd6f
juy4556 <kws05050@naver.com>
2023-05-07 10:41:32 +0900

    23.05.07

--- 백준/14501 퇴사.py
+++ 백준/14501 퇴사.py
@@ -1,15 +1,15 @@
 N = int(input())
-dp = [0] * (N+1)
-T=[]
-P=[]
-for i in range(N-1, -1, -1):
+dp = [0] * (N + 1)
+T = []
+P = []
+for i in range(N - 1, -1, -1):
     t, p = map(int, input().split())
     T.append(t)
     P.append(p)
 
-for i in range(N-1, -1, -1):
-    if T[i]+i > N:
-        dp[i] = dp[i+1]
+for i in range(N - 1, -1, -1):
+    if T[i] + i > N:
+        dp[i] = dp[i + 1]
     else:
-        dp[i] = max(dp[i+T[i]]+P[i], dp[i+1])
+        dp[i] = max(dp[i + T[i]] + P[i], dp[i + 1])
 print(dp[0])
