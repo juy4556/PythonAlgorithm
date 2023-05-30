8da953b6db9d3bdeade75d0418adf7141a9399e9
juy4556 <kws05050@naver.com>
2023-05-30 17:39:05 +0900

    23.05.30

---
+++ 백준/15989 1, 2, 3 더하기 4.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    T = int(input())
+    dp = [0] * 10001
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    for i in range(4, 10001):
+        dp[i] = dp[i-3] + i//2 + 1
+
+    for _ in range(T):
+        n = int(input())
+        print(dp[n])
