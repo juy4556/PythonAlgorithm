70d77608b4c62686f363c4178a55b24b2f2359ea
juy4556 <kws05050@naver.com>
2023-05-31 15:23:59 +0900

    23.05.31

---
+++ 백준/15992 1, 2, 3 더하기 7.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    T = int(input())
+    dp = [[0 for _ in range(1001)] for _ in range(1001)]
+    dp[1][1] = 1
+    dp[2][1] = 1
+    dp[2][2] = 1
+    dp[3][1] = 1
+    dp[3][2] = 2
+    dp[3][3] = 1
+    for i in range(4, 1001):
+        for j in range(1, 1001):
+            dp[i][j] = (dp[i-1][j-1] + dp[i-2][j-1] + dp[i-3][j-1]) % 1000000009
+
+    for _ in range(T):
+        n, m = map(int, input().split())
+        print(dp[n][m] % 1000000009)
