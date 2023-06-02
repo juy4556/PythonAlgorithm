3d3488e9acfcdaa84df4cfe4b2a9c455d60e01cc
juy4556 <kws05050@naver.com>
2023-06-02 16:45:51 +0900

    23.06.02

---
+++ 코드트리/1,2,3 더하기 6.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    dp = [[0 for _ in range(1001)] for _ in range(1001)]
+    dp[1][1] = 1
+    dp[2][1:3] = [1, 1]
+    dp[3][1:4] = [1, 2, 1]
+
+    for i in range(4, 1001):
+        for j in range(1, 1001):
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 2][j - 1] + dp[i - 3][j - 1]) % 1000000007
+
+    n, m = map(int, (input().split()))
+    print(dp[n][m] % 1000000007)
