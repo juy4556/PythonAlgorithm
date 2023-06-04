f532015f266eacabf6d02d140601c6511031a1b1
juy4556 <kws05050@naver.com>
2023-06-04 20:16:09 +0900

    23.06.04

--- 백준/1629 곱셈.py
+++ 백준/1629 곱셈.py
@@ -6,13 +6,13 @@ input = sys.stdin.readline
 def sol(a, b, c):
     if b == 1:
         return a % c
-    temp = sol(a, b//2, c)
+    temp = sol(a, b // 2, c)
     if b % 2:
-        return (temp * temp * a) % c
+        return (temp * temp * (a % c)) % c
     else:
         return (temp * temp) % c
 
 
 if __name__ == "__main__":
     a, b, c = map(int, input().split())
-    print(sol(a,b,c))
+    print(sol(a, b, c))
