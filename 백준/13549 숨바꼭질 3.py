da39bbc96745bc753dc01e7cf5769cf4131d2dce
juy4556 <kws05050@naver.com>
2023-06-22 20:14:59 +0900

    23.06.22

---
+++ 백준/13549 숨바꼭질 3.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+def bfs(n):
+    global count
+    q = deque([(n, 0)])
+    visited[n] = 1
+    while q:
+        num, cnt = q.popleft()
+        visited[num] = 1
+        if cnt > count:
+            break
+        if num == K:
+            count = min(count, cnt)
+            continue
+        a, b, c = num - 1, num + 1, num * 2
+        if c <= 100000 and not visited[c]:
+            q.append((c, cnt))
+        if a >= 0 and not visited[a]:
+            q.append((a, cnt + 1))
+        if b <= 100000 and not visited[b]:
+            q.append((b, cnt + 1))
+
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    visited = [0] * 100001
+    count = abs(K - N)
+    bfs(N)
+    print(count)
