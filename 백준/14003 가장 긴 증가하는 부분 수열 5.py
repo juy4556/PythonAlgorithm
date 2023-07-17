cec8993d07386048a41bfdb8c01a95edfb5f32a2
juy4556 <kws05050@naver.com>
2023-07-17 17:35:37 +0900

    23.07.17

--- 백준/14003 가장 긴 증가하는 부분 수열 5.py
+++ 백준/14003 가장 긴 증가하는 부분 수열 5.py
@@ -1,4 +1,5 @@
-def bin_search(arr, target, start, end):
+def bin_search(arr, target):
+    start, end = 0, len(arr) - 1
     while start <= end:
         mid = (start + end) // 2
         if arr[mid][1] >= target:
@@ -11,25 +12,25 @@ def bin_search(arr, target, start, end):
 if __name__ == "__main__":
     N = int(input())
     A = list(map(int, input().split()))
-    arr = [[0, A[0]]]  # 수열의 인덱스와 값을 함께 리스트에 저장
-    result = [[0, A[0]]]
-
-    for i in range(1, N):
-        if arr[-1][1] < A[i]:  # 수열 값이 리스트 마지막 요소의 값보다 크면 추가
-            arr.append([i, A[i]])
-            result = arr[::]
+    seq = [[0, A[0]]]
+    res = [[0, A[0]]]
+    result = []
+    for i in range(1, len(A)):
+        if seq[-1][1] < A[i]:
+            length = len(seq)
+            seq.append([length, A[i]])
+            res.append([length, A[i]])
             continue
-        index = bin_search(arr, A[i], 0, len(arr) - 1)
-        if arr[index][1] > A[i]:  # 이중탐색으로 얻은 리스트의 값이 수열의 값보다 클 때만 리스트 요소 초기화
-            arr[index] = [i, A[i]]
+        index = bin_search(seq, A[i])
+        seq[index][1] = A[i]
+        res.append([index, A[i]])
 
-    result = [[-1, -int(1e9)]] + result
-
-    for i in range(len(result) - 1):
-        if result[i][0] > result[i + 1][0]:  # result 리스트에 인덱스[0]의 값(수열 요소값)이 다음 인덱스의 값보다 클 때
-            for j in range(result[i - 1][0] + 1, result[i + 1][0]):  # 이전 인덱스의 요소 값 ~ 다음 인덱스 요소 값 사이에서
-                if A[j] < result[i + 1][1]:  # 다음 인덱스의 값보다 작은 수열 값 찾아서 초기화
-                    result[i] = [j, A[j]]
-
-    print(len(result) - 1)
-    print(*[result[i][1] for i in range(1, len(result))])
+    index = len(seq) - 1
+    for idx, val in res[::-1]:
+        if idx == index:
+            result.append(val)
+            index -= 1
+        if index < 0:
+            break
+    print(len(seq))
+    print(*result[::-1])
---
+++ 이진 탐색/이진탐색 알고리즘 정리.py
@@ -0,0 +1,31 @@
+from bisect import bisect_left, bisect_right
+
+
+def bin_search1(arr, target):
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] >= target:
+            end = mid - 1
+            continue
+        start = mid + 1
+    return start, end
+
+
+def bin_search2(arr, target):
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] > target:
+            end = mid - 1
+            continue
+        start = mid + 1
+    return start, end
+
+
+a = [1, 63, 46, 36, 246, 43, 57, 63, 34, 6578, 467, 534, 23, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 11, 11, 1, 1, 2]
+a.sort()
+print(a)
+print(bisect_left(a, 1), bisect_right(a, 1))
+print(bin_search1(a, 1))
+print(bin_search2(a, 1))
